bbdadbda93396eb1b99ee47385a2407672b410b3
benkul <benkul@gmail.com>
2014-07-30 16:31:56 -0700

    created django project

--- poetrygen.py
+++ poetrygen.py
@@ -1,10 +1,20 @@
 import bacon
 
 
-window = game_function()
+
 bacon.window.width = 640
 bacon.window.height = 540
 bacon.window.fullscreen = False
 
 class Interface(bacon.Game):
 	def on_tick(self):
+		bacon.push_color()
+		bacon.set_color(0, 0, 0, 1)
+		bacon.draw_rect(0,0, 640, 540)
+		bacon.pop_color()
+		bacon.set_color(1, 1, 1, 1)
+		bacon.draw_rect(10, 10, 630, 530)
+		bacon.pop_color()
+
+
+bacon.run(Interface())
