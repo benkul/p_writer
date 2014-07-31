0deb059674989d5da50b80c84140a1afe04792db
benkul <benkul@gmail.com>
2014-07-31 11:32:21 -0700

    editing models

--- pwriter/poetic/models.py
+++ pwriter/poetic/models.py
@@ -7,6 +7,7 @@ class Poem(models.Model):
     title = models.CharField(max_length=120)
     num_lines = models.IntegerField(default=randrange(3, 7))
     num_words_per_line = models.IntegerField(default=randrange(4, 8))
+    author = models.CharField(max_length=120)
 
     def __unicode__(self):
         return self.title
@@ -14,4 +15,26 @@ class Poem(models.Model):
     def poem_stats(self):
         return "number of lines: %r  number of words per line: %r" % (self.num_lines, self.num_words_per_line)
 
+    def return_poem(self):
+        byline = "%s by %s\n" % (self.title, self.author)
+
+
+        return byline + poetry
+
+
+
+class Line(models.Model):
+    poem_part = models.ForeignKey(Poem)
+    poem_line = models.CharField(max_length=200)
+    line_number = models.IntegerField()
+
+    def __unicode__(self):
+        return self.poem_line
+
+    def line_num_line(self):
+        line = {self.line_number : self.poem_line}
+        return line
+
+    def return_poem(self, value):
+        if value == self.poem_part:
 
