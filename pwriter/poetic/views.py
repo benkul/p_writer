1e3c4e2a93b4cfa47b20e7f04092693c7f886fb8
benkul <benkul@gmail.com>
2014-07-31 08:54:18 -0700

    trying to find index

--- pwriter/poetic/views.py
+++ pwriter/poetic/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
 from django.template import RequestContext
-#from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response
 
 def index(request):
     context = RequestContext(request)
-    return render('generator/index.html', context)
+    return render_to_response('poetic/index.html', context)
 
