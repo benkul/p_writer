92807d75015879f596dfdf9f1addb20413e778a3
benkul <benkul@gmail.com>
2014-07-31 09:13:02 -0700

    changes

--- pwriter/poetic/models.py
+++ pwriter/poetic/models.py
@@ -4,7 +4,14 @@ from random import randrange
 
 # Create your models here.
 class Poem(models.Model):
+    title = models.CharField(max_length=120)
     num_lines = models.IntegerField(default=randrange(3, 7))
     num_words_per_line = models.IntegerField(default=randrange(4, 8))
+
+    def __unicode__(self):
+        return self.title
+
+    def poem_stats(self):
+        return "number of lines: %r  number of words per line: %r" % (self.num_lines, self.num_words_per_line)
 
 
